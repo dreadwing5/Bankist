7ae65e3972a0d0a571614d5aff3471131db5547c
dreadwing5 <sachinspyk@outlook.com>
2021-02-02 02:13:11 +0530

    Add Smooth Scroll

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-"use strict";
+'use strict';
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".btn--close-modal");
-const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
 
-const header = document.querySelector(".header");
+const header = document.querySelector('.header');
 
-const message = document.createElement("div");
-message.classList.add("cookie-message");
+const message = document.createElement('div');
+message.classList.add('cookie-message');
 
 message.innerHTML =
   'We use cookie for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
@@ -42,7 +42,15 @@ header.append(message);
 
 //Delete Element
 document
-  .querySelector(".btn--close-cookie")
-  .addEventListener("click", function () {
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
     message.remove();
   });
+
+//Smooth Scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
