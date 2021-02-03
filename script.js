b217d49e9f1a17c4cdee54ea0edc1d7530bec3bc
dreadwing5 <sachinspyk@outlook.com>
2021-02-03 22:41:04 +0530

    Add slider and defer attribute to script

--- script.js
+++ script.js
@@ -153,7 +153,6 @@ allSections.forEach(function (section) {
 
 //Lazy Loading images
 
-
 const loadImg = function (entries, observer) {
   const [entry] = entries;
   const img = entry.target;
@@ -176,3 +175,86 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+//Slider
+
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const dotContainer = document.querySelector('.dots');
+  const btnRight = document.querySelector('.slider__btn--right');
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDots = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  const gotoSlide = function (curSlide) {
+    slides.forEach(
+      (s, i) =>
+        (s.style.transform = `translateX(${
+          100 * (i - (curSlide % maxSlide))
+        }%)`)
+    );
+  };
+
+  //Initial Position
+
+  //Prev SLide
+  const prevSlide = function () {
+    if (curSlide === 0) curSlide = maxSlide;
+    curSlide--;
+    gotoSlide(curSlide);
+    activateDots(curSlide);
+  };
+  //Next slide
+  const nextSlide = function () {
+    curSlide++;
+    gotoSlide(curSlide);
+    activateDots(curSlide % maxSlide);
+  };
+
+  //Init
+  const init = function () {
+    gotoSlide(0);
+    createDots();
+    activateDots(0);
+  };
+  init();
+
+  //Event Handlers
+
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  document.addEventListener('keydown', function (e) {
+    e.key === 'ArrowRight' && nextSlide();
+  });
+  document.addEventListener('keydown', function (e) {
+    e.key === 'ArrowLeft' && prevSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (!e.target.classList.contains('dots__dot')) return;
+    const slideIndex = e.target.dataset.slide;
+    gotoSlide(slideIndex);
+    activateDots(slideIndex);
+  });
+};
+slider();
